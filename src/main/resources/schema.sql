87900d385724fde4ac671a5d75e8c1031a1842c9
백재우 <9712jw@gmail.com>
2025-03-09 10:54:21 +0900

    Create  user status 테이블 스키마 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,34 @@
 -- USER
 CREATE TABLE users
 (
-    user_id UUID,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(60) NOT NULL,
-    CONSTRAINT
+    user_id    UUID,
+    created_at TIMESTAMP    NOT NULL,
+    updated_at TIMESTAMP    NOT NULL,
+    username   VARCHAR(50)  NOT NULL,
+    email      VARCHAR(100) NOT NULL,
+    password   VARCHAR(60)  NOT NULL,
+    profile_id UUID,
+    CONSTRAINT pk_users_user_id PRIMARY KEY (user_id),
+    CONSTRAINT uk_users_username UNIQUE (username),
+    CONSTRAINT uk_users_email UNIQUE (email),
+    CONSTRAINT fk_user_statuses_users
+        FOREIGN KEY (profile_id)
+            REFERENCES user_statuses (user_statuse_id)
+            ON DELETE SET NULL
 );
 
+-- USER STATUSES
+CREATE TABLE user_statuses
+(
+    user_statuses_id UUID,
+    created_at       TIMESTAMP NOT NULL,
+    updated_at       TIMESTAMP,
+    user_id          UUID,
+    last_active_at   TIMESTAMP NOT NULL,
+    CONSTRAINT pk_user_statuses_id PRIMARY KEY (user_statuses_id),
+    CONSTRAINT fk_users_user_statuses
+        FOREIGN KEY (user_id)
+            REFERENCES users (user_id) ON DELETE CASCADE
+);
 
-
-
+-- BINARY_CONTENT
